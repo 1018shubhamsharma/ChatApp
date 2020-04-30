b721a5dc2706951ee4e1f7951f33335c23e45b77
Shubham <1018shubhamsharma@gmail.com>
2020-04-30 13:02:14 +0530

    finally project is finished

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,50 @@
 let socket = io();
 
+function scrollToBottom() {
+  let messages = document.querySelector('#messages').lastElementChild;
+  messages.scrollIntoView();
+}
+
 socket.on("connect" , function()  {
-  console.log("Connected to server");
+   let searchQuery = window.location.search.substring(1);
+  let params = JSON.parse('{"' + decodeURI(searchQuery).replace(/&/g, '","').replace(/\+/g, ' ').replace(/=/g,'":"') + '"}');
+
+  socket.emit('join', params, function(err) {
+    if(err){
+      alert(err);
+      window.location.href = '/';
+    }else {
+      console.log('No Error');
+    }
+  })
 });
 
 socket.on("disconnect" , function() {
   console.log("disconnected from server");
 });
 
+socket.on('updateUsersList', function (users) {
+  let ol = document.createElement('ol');
+
+  users.forEach(function (user) {
+    let li = document.createElement('li');
+    li.innerHTML = user;
+    ol.appendChild(li);
+  });
+
+  let usersList = document.querySelector('#users');
+  usersList.innerHTML = "";
+  usersList.appendChild(ol);
+})
+
 socket.on('newMessage',function(message,){
-    console.log(message);
     let li = document.createElement('li');
     li.innerText = message.from +  " : " + message.text;
+    const div = document.createElement('div');
+    div.innerHTML = li;
     document.querySelector('body').appendChild(li);
+    document.querySelector('#messages').appendChild(div);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(pos){
@@ -23,20 +55,25 @@ socket.on('newLocationMessage',function(pos){
     a.setAttribute('href' , pos.url);
     li.innerText = pos.from + " : ";
     li.appendChild(a);
-    document.querySelector('body').appendChild(li);
+    const div = document.createElement('div');
+    div.innerHTML = li
+
+   document.querySelector('#messages').appendChild(div);
+   scrollToBottom();
+    
 });
 
-document.querySelector('#btn_1').addEventListener('click',function(e){
+document.querySelector('#submit-btn').addEventListener('click',function(e){
   e.preventDefault();
 
   socket.emit('createdMessage',{
     from : "user",
-    text : document.querySelector("input[name='text1']").value,
+    text : document.querySelector("input[name='message']").value,
     createdAt : new Date().getTime()
   });
 });
 
-document.querySelector("#btn_loc").addEventListener('click',function(){
+document.querySelector("#send-location").addEventListener('click',function(){
     if(!navigator.geolocation)
     {
       return console.log('Browser does not support geolocation.')
